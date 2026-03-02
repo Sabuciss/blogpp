dddcc1971dac452340ba79c872a3696e3657adf0
unknown <ipb23.s.brikmane@vtdt.edu.lv>
2026-03-02 10:16:29 +0200

    fvjniojr

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -3,20 +3,74 @@
 require "models/Blog.php";
 
 class BlogController {
-  
     public function index() {
         $posts = Blog::all();
         require "views/blog/index.view.php";
-      }
+    }
 
-      public function show($id) {
+    public function show($id) {
         $post = Blog::find($id);
+
         if (!$post) {
             http_response_code(404);
             echo "Ieraksts nav atrasts!";
             return;
         }
+
         require "views/blog/show.view.php";
     }
+
+    public function create() {
+        require "views/blog/create.view.php";
+    }
+
+    public function store() {
+        Blog::create($_POST);
+        $this->redirect('/');
+    }
+
+    public function edit($id) {
+        $post = Blog::find($id);
+
+        if (!$post) {
+            http_response_code(404);
+            echo "Ieraksts nav atrasts!";
+            return;
+        }
+
+        require "views/blog/edit.view.php";
+    }
+
+    public function update($id) {
+        $post = Blog::find($id);
+
+        if (!$post) {
+            http_response_code(404);
+            echo "Ieraksts nav atrasts!";
+            return;
+        }
+
+        $post->body = trim($_POST['body'] ?? '');
+        $post->save();
+
+        $this->redirect('/posts/' . $id);
+    }
+
+    public function destroy($id) {
+        $post = Blog::find($id);
+
+        if ($post) {
+            $post->delete();
+        }
+
+        $this->redirect('/');
+    }
+
+    private function redirect($path) {
+        header('Location: ' . $path);
+        exit();
+    }
+
+
+
 }
-  
